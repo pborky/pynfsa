57d26b36a254a167e7cf15b341a504628082342f
Peter Boraros <pborky@pborky.sk>
2012-12-18 07:57:09 +0100

    Add some informative messages.

---
+++ info.py
@@ -0,0 +1,7 @@
+__author__ = 'Peter Boraros'
+__date__ = '2012-12-18'
+__prog__ = 'NetFlow Spectral Analyser'
+__version__ = '0.1'
+__description__='''NetFlow Spectral Analyser - experimental software for
+anomaly detection using spectral analysis on pcap and netflow data.
+'''
